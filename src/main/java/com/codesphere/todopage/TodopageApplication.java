89d893975fc594e245738ed9f04f2ee2fd64737d
Adriel Ryan <adrielryan.tj@hotmail.com>
2024-02-02 23:51:40 -0300

    Estruturação inicial do projeto

---
+++ src/main/java/com/codesphere/todopage/TodopageApplication.java
@@ -0,0 +1,13 @@
+package com.codesphere.todopage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodopageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodopageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codesphere/todopage/TodopageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesphere.todopage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodopageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
